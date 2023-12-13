7aef7adc4e01a2c17a1a35b4be164a35b470a906
LuciAkirami <ajsubscriber001@gmail.com>
2023-12-13 20:05:21 +0530

    added MultiHeadAttention

--- transformer-from-scratch/model.py
+++ transformer-from-scratch/model.py
@@ -206,3 +206,101 @@ class FeedForwardLayer(nn.Module):
         x = self.linear_2(x)
 
         return x
+    
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model: int, h: int, dropout: float):
+        """
+        Initialize the MultiHeadAttention model.
+
+        Args:
+            d_model (int): The embedding size.
+            h (int): The number of attention heads.
+            dropout (float): The dropout rate.
+
+        Returns:
+            None
+        """
+        super.__init__()
+        self.d_model = d_model
+        self.h = h
+
+        # The dimension of each attention head
+        self.d_k = d_model // h
+
+        # Define the linear layers for query, key, value, and output
+        self.w_q = nn.Linear(d_model,d_model) #Wq
+        self.w_k = nn.Linear(d_model,d_model) #Wk
+        self.w_v = nn.Linear(d_model,d_model) #Wv
+        self.w_o = nn.Linear(d_model,d_model) #Wo
+
+        # Define the dropout layer
+        self.dropout = nn.Dropout(dropout)
+
+    def attention(self, query, key, value, dropout=None, mask=None):
+        """
+        Calculate the attention scores.
+
+        Args:
+            query (torch.Tensor): The query tensor.
+            key (torch.Tensor): The key tensor.
+            value (torch.Tensor): The value tensor.
+            dropout (nn.Module, optional): The dropout layer. Default is None.
+            mask (torch.Tensor, optional): The mask tensor. Default is None.
+
+        Returns:
+            tuple: The output tensor and the attention scores.
+        """
+
+        # Calculate the attention scores
+        # (Batch_size,h,seq_len,d_k) --> (Batch_size,h,seq_len,seq_len)
+        attention_scores = (query @ key.transpose(-2,-1)) / math.sqrt(self.d_k)
+
+        # Apply the mask if provided
+        # masking is needed in decoder, where the attention of current word should only be calculated from the previous words but not the next word
+        if mask is not None:
+            attention_scores.masked_fill_(mask==0,-1e9)
+
+        # Apply the softmax function to the attention scores
+        attention_scores = attention_scores.softmax(dim=-1) # (Batch_size,h,seq_len,seq_len)
+
+        # Apply dropout if provided
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        # returning both final attention and attention_scores
+        return (attention_scores @ value), attention_scores
+
+    def forward(self, q, k, v):
+        """
+        Forward pass of the model.
+
+        Args:
+            q (torch.Tensor): The query tensor.
+            k (torch.Tensor): The key tensor.
+            v (torch.Tensor): The value tensor.
+
+        Returns:
+            torch.Tensor: The output tensor.
+        """
+
+        # Pass the input tensors through the linear layers
+        query = self.w_q(q) # (Batch_size,seq_len,d_model) --> (Batch_size,seq_len,d_model)
+        key = self.w_k(k) # (Batch_size,seq_len,d_model) --> (Batch_size,seq_len,d_model)
+        value = self.w_v(v) # (Batch_size,seq_len,d_model) --> (Batch_size,seq_len,d_model)
+
+        # Reshape and transpose the tensors for multi-head attention
+        # (Batch_size,seq_len,d_model) --> (Batch_size,seq_len,h,d_k) -->  (Batch_size,h,seq_len,d_k)
+        query = query.view(query.shape[0],query.shape[1],self.h,self.d_k).transpose(1,2)
+        key = key.view(key.shape[0],key.shape[1],self.h,self.d_k).transpose(1,2)
+        value = value.view(value.shape[0],value.shape[1],self.h,self.d_k).transpose(1,2)
+
+        # Calculate the attention
+        x, attention_scores = MultiHeadAttention.attention(query,key,value,self.dropout)
+        
+        # Reshape and transpose the output tensor
+        # Batch_size,h,seq_len,d_k) --> (Batch_size,seq_len,d_model)
+        x = x.transpose(1,2).contigous().view(x.shape[0],-1,self.d_k * self.h)
+
+        # Pass the output tensor through the output linear layer
+        # (Batch_size,seq_len,d_model) --> (Batch_size,seq_len,d_model)
+        return self.w_o(x)
